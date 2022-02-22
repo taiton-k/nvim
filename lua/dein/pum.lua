1238042d1b8560a98b0f80cd32762d2e89224074
taiton-k <84013946+taiton-k@users.noreply.github.com>
2022-02-22 15:51:44 +0900

    when CursorMovedI, pum-window will be closed.

--- lua/dein/pum.lua
+++ lua/dein/pum.lua
@@ -1,8 +1,13 @@
-local g = vim.g;
+--local g = vim.g;
 local fn = vim.fn;
+local cmd = vim.cmd;
 
 fn['pum#set_option']({
         border = 'rounded';
         highlight_normal_menu = 'LocalFloating';
         highlight_selected = 'LocalSelected';
 });
+
+cmd([[
+autocmd CursorMovedI * call pum#close()
+]])
