d5159c499f07c7f75c6ffd820a81fe959dc709f8
taiton-k <84013946+taiton-k@users.noreply.github.com>
2022-03-10 11:03:30 +0900

    nyan

--- lua/configs.lua
+++ lua/configs.lua
@@ -64,19 +64,14 @@ api.nvim_add_user_command('Acc','FlTermRun acc <args>',{nargs = '*'});
 
 
 -- Emacs Keybinds
-ikey('<C-b>','<Left>',{noremap = true});
-ikey('<C-f>','<Right>',{noremap = true});
-ikey('<C-p>','<Up>',{noremap = true});
-ikey('<C-n>','<Down>',{noremap = true});
+-- For completion plugin (ddc.vim)
+ikey('<C-b>','<Left><Cmd>call pum#map#cancel()<CR>',{noremap = true});
+ikey('<C-f>','<Right><Cmd>call pum#map#cancel()<CR>',{noremap = true});
+ikey('<C-p>','<Up><Cmd>call pum#map#cancel()<CR>',{noremap = true});
+ikey('<C-n>','<Down><Cmd>call pum#map#cancel()<CR>',{noremap = true});
+
 ikey('<C-a>','<Home><S-Right>',{noremap = true});
 ikey('<C-e>','<End>',{noremap = true});
-
--- For completion
-ikey('<C-b>','<Cmd>call pum#map#cancel()<CR>',{noremap = true});
-ikey('<C-f>','<Cmd>call pum#map#cancel()<CR>',{noremap = true});
-ikey('<C-p>','<Cmd>call pum#map#cancel()<CR>',{noremap = true});
-ikey('<C-n>','<Cmd>call pum#map#cancel()<CR>',{noremap = true});
-
 
 
 
