32ce5ee240ecc6a901381f506e41e479aeebeb5b
taiton-k <84013946+taiton-k@users.noreply.github.com>
2022-07-07 18:13:52 +0900

    update indentblankline configuration

--- lua/dein/indent_blankline.lua
+++ lua/dein/indent_blankline.lua
@@ -38,6 +38,7 @@ require('indent_blankline').setup({
                 'SpaceCharPurple',
         };
         use_treesitter = true;
+        show_trailing_blankline_indent = false;
         filetype_exclude = {'help'},
         buftype_exclude = {'terminal'},
 });
