df939cff663f90854c155d387bc4ad1ec0ce5f9c
taiton-k <84013946+taiton-k@users.noreply.github.com>
2022-07-23 20:43:09 +0900

    refactor it

--- lua/configs/commands.lua
+++ lua/configs/commands.lua
@@ -1,8 +1,9 @@
 local command = vim.api.nvim_create_user_command
 command('Help', 'vert help <args>', {nargs = '*', complete = 'help'})
 
-command('OjTest', 'TermExec cmd="oj t -N -d tests"', {})
-command('Submit', 'TermExec cmd="quom main.cpp submit.cpp -I ~/Documents/KyouPuro/c++/ && acc submit"', {})
-command('Acc', 'TermExec cmd="acc <args>"', {nargs = '*'})
-command('Execute', 'TermExec cmd="./a.out"', {})
-command('Compile', 'TermExec cmd="make <args>"', {nargs = '*'})
+command('RunCommand', 'TermExec cmd="<args>" go_back=0', {nargs = "*"})
+command('OjTest', 'RunCommand oj t -N -d tests', {})
+command('Submit', 'RunCommand quom main.cpp submit.cpp -I ~/Documents/KyouPuro/c++/ && acc submit', {})
+command('Acc', 'RunCommand acc <args>', {nargs = '*'})
+command('Execute', 'RunCommand ./a.out', {})
+command('Compile', 'RunCommand make <args>', {nargs = '*'})
