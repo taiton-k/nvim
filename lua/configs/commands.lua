d285c1bb4108ab4f18cb332f606dfacd60cc669f
taiton-k <84013946+taiton-k@users.noreply.github.com>
2022-07-23 18:38:28 +0900

    add commands for competitive programming

--- lua/configs/commands.lua
+++ lua/configs/commands.lua
@@ -1,1 +1,8 @@
-vim.api.nvim_create_user_command('Help', 'vert help <args>', {nargs = '*',complete = 'help'});
+local command = vim.api.nvim_create_user_command
+command('Help', 'vert help <args>', {nargs = '*', complete = 'help'})
+
+command('OjTest', 'TermExec cmd="oj t -N -d tests"', {})
+command('Submit', 'TermExec cmd="quom main.cpp submit.cpp -I ~/Documents/KyouPuro/c++/ && acc submit"', {})
+command('Acc', 'TermExec cmd="acc <args>"', {nargs = '*'})
+command('Execute', 'TermExec cmd="./a.out"', {})
+command('Compile', 'TermExec cmd="make <args>"', {nargs = '*'})
